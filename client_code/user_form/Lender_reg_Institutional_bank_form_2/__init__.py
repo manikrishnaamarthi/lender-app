8a172078e9a6a0588484edef894deef48a71a50e
bejawadaprem@gmail.com <bejawadaprem@gmail.com>
2023-11-01 08:41:37 +0000

    Created user_form.Lender_reg_Institutional_bank_form_1, user_form.Lender_reg_Institutional_bank_form_2, user_form.Lender_reg_Institutional_form_1, user_form.Lender_reg_Institutional_form_2 and 9 more

---
+++ client_code/user_form/Lender_reg_Institutional_bank_form_2/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import Lender_reg_Institutional_bank_form_2Template
+from anvil import *
+
+class Lender_reg_Institutional_bank_form_2(Lender_reg_Institutional_bank_form_2Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    open_form('user_form.Lender_reg_form_6')
+    """This method is called when the button is clicked"""
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
---
+++ client_code/user_form/Lender_reg_Institutional_form_4/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import Lender_reg_Institutional_form_4Template
+from anvil import *
+
+class Lender_reg_Institutional_form_4(Lender_reg_Institutional_form_4Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    open_form('user_form.Lender_reg_form_6')
+    """This method is called when the button is clicked"""
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
